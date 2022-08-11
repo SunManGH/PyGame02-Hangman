46ec177452103dfa1c925a9c683817885fadbf25
Sunil <v-2smanj@hotmail.com>
2022-08-12 02:15:38 +0530

    added photos

--- game0.4.py
+++ game0.4.py
@@ -1,7 +1,7 @@
-# Set a word to guess
-# always just center the text rectangle when you grab it
+# Set hangman art and lives
 
 import pygame
+import hangman_art
 
 # initilize the game
 pygame.init()
@@ -19,6 +19,12 @@ theWord = 'Welcome'
 # Get length of word
 Word_Label = '_ '*len(theWord)
 
+# Set Lives
+lives=6
+
+# Set image
+hangmanImage = pygame.image.load('LivesLeft_6_br.png')
+
 # Formatting text
 def format_my_text(myText, fontName: str = 'Century',fontSize: int = 25,fontColor = (19,61,87)):
     f = pygame.font.SysFont(fontName, fontSize)
@@ -31,7 +37,8 @@ guessTheWord_Label = format_my_text('GUESS THE WORD')
 tries_Label = format_my_text('Tries : ')
 Word_Label = format_my_text(Word_Label,fontSize=100)
 # always just center the text rectangle when you grab it
-Word_Label_rect = Word_Label.get_rect(center=(400,300))
+Word_Label_rect = Word_Label.get_rect(center=(500,300))
+
 
 # Game loop
 running = True
@@ -41,6 +48,7 @@ while running:
     screen.fill((245,180,26))
 
     # font color
+    screen.blit(hangmanImage,(0,200))
     screen.blit(Heading_Label,(250,200))
     screen.blit(guessTheWord_Label,(560,50))
     screen.blit(tries_Label,(30,50))
